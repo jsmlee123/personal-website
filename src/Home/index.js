4fdad011ac4aa0e81a8eb2f78ecd73d3e24e07de
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2024-01-08 20:02:22 -0500

    Home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,34 @@
+import React, {useState} from 'react';
+import "./index.css";
+
 
 const Home = () => {
+    const [isProfileHover, setProfileHover] = useState(false);
+
     return (
-        <div className="ms-5">
-            Hello
+        <div className="d-flex flex-column align-items-center justify-content-center home-container mt-5">
+            <div className="d-flex flex-row align-items-center justify-content-left home-card rounded-5 overflow-auto"> 
+                <div className={`d-flex align-items-center justify-content-center pfp rounded-4`} 
+                    onMouseEnter={() => setProfileHover(true)}
+                    onMouseLeave={() => setProfileHover(false)}
+                >
+                    {isProfileHover && (
+                        <div className='pfp-text text-white rounded-4'>
+                            Corgi Corki
+                        </div>
+                    )}
+                </div>
+                <div className='ms-5 text-body-card rounded-5 comic-sans'>
+                    <h1>Intro</h1>
+                    <p className='mt-4 pe-3'>
+                        Hi! I'm Jonathan and this is my personal website. I'm a software engineer
+                        who graduated from Northeastern University. I have a passion for writing 
+                        understandable, simple code and in my free time you can catch me playing
+                        tennis or video games. (I'm a corki one trick)
+
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,8 +1,5 @@
 import React, {useState} from 'react';
-import { RiDashboard3Line } from "react-icons/ri";
-import { MdOutlineAccountCircle } from "react-icons/md";
-import { FaHome, FaSearch, FaSignInAlt } from "react-icons/fa";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 
@@ -19,7 +16,7 @@ const Nav = () => {
     ];
 
     return (
-        <div className='d-flex flex-grow-1 side-bar justify-content-between rounded-4 mt-2'>
+        <div className='d-flex flex-grow-1 side-bar justify-content-between rounded-4 mt-3'>
             <div className='d-inline-flex align-items-center ps-5'>
                 <Link
                     key={"name_header"}
